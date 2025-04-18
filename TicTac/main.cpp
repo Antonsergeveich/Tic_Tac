5aebc4f64683ec94858d19097358344e9a2dc929
Persh_vx33@top-academy.ru <Persh_vx33@top-academy.ru>
2025-04-18 15:56:52 +0300

    +

--- TicTac/main.cpp
+++ TicTac/main.cpp
@@ -26,10 +26,19 @@ int main()
 		printBoard();
 
 		playerMove();
+		winner = checkWinner();
+		if (winner != ' ' || checkFreeSpaces() == 0)
+		{
+			break;
+		}
+
+		computerMove();
+		winner = checkWinner();
+		if (winner != ' ' || checkFreeSpaces() == 0)
+		{
+			break;
+		}
 	}
-
-	printBoard();
-
 
 	return 0;
 }
@@ -74,31 +83,90 @@ void playerMove()
 {
 	int x;
 	int y;
-	printf("Enter row #(1-3): ");
-	scanf("%d", &x);
-	x--;
-	printf("Enter column #(1-3): ");
-	scanf("%d", &y);
-	y--;
+	do
+	{
+		printf("Enter row #(1-3): ");
+		scanf("%d", &x);
+		x--;
+		printf("Enter column #(1-3): ");
+		scanf("%d", &y);
+		y--;
 
-	if (board[x][y] != ' ') 
-	{
-		printf("Invalid move!\n");
-	}
-	else
-	{
-		board[x][y] = PLAYER;
-	}
+		if (board[x][y] != ' ')
+		{
+			printf("Invalid move!\n");
+		}
+		else
+		{
+			board[x][y] = PLAYER;
+			break;
+		}
+	} while (board[x][y] != ' ');
 }
 void computerMove()
 {
+	//creates a seed based on current time
+	srand(time(0));
+	int x;
+	int y;
 
+	if (checkFreeSpaces() > 0)
+	{
+		do
+		{
+			x = rand() % 3;
+			y = rand() % 3;
+		} while (board[x][y] != ' ');
+
+		board[x][y] = COMPUTER;
+	}
+	else
+	{
+		printWinner(' ');
+	}
 }
 char checkWinner()
 {
-	return 'O';
+	//check rows
+	for (int i = 0; i < 3; i++) 
+	{
+		if (board[i][0] == board[i][1] && board[i][0] == board[i][2]) 
+		{
+			return board[i][0];
+		}
+	}
+	//check columns
+	for (int i = 0; i < 3; i++)
+	{
+		if (board[0][i] == board[1][i] && board[0][i] == board[2][i])
+		{
+			return board[0][i];
+		}
+	}
+	//check diagonals
+		if (board[0][0] == board[1][1] && board[0][0] == board[2][2])
+		{
+			return board[0][0];
+		}
+		if (board[0][2] == board[1][1] && board[0][2] == board[2][0])
+		{
+			return board[0][2];
+		}
+
+		return ' ';
 }
 void printWinner(char winner)
 {
-
+	if (winner == PLAYER) 
+	{
+		printf("YOU WIN!");
+	}
+	else if(winner == COMPUTER)
+	{
+		printf("YOU LOSE!");
+	}
+	else
+	{
+		printf("It is a tie.");
+	}
 }
