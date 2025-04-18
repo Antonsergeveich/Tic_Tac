577cefd83fd9db2bd7101260303c3a0523ef4d88
Persh_vx33@top-academy.ru <Persh_vx33@top-academy.ru>
2025-04-18 15:10:33 +0300

    +

--- TicTac/main.cpp
+++ TicTac/main.cpp
@@ -3,6 +3,10 @@
 #include <ctype.h>
 #include <time.h>
 
+char board[3][3];
+const char PLAYER = 'X';
+const char COMPUTER = 'O';
+
 void resetBoard();
 void printBoard();
 int checkFreeSpaces();
@@ -13,35 +17,52 @@ void printWinner(char);
 
 int main()
 {
+	char winner = ' ';
+
+	resetBoard();
+	printBoard();
+
 
 	return 0;
 }
 
 void resetBoard()
 {
-
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			board[i][j] = ' ';
+		}
+	}
 }
 void printBoard()
 {
-
+	printf(" %c | %c | %c ", board[0][0], board[0][1], board[0][2]);
+	printf("\n---|---|---\n");
+	printf(" %c | %c | %c ", board[1][0], board[1][1], board[1][2]);
+	printf("\n---|---|---\n");
+	printf(" %c | %c | %c ", board[2][0], board[2][1], board[2][2]);
+	printf("\n---|---|---\n");
+	printf("\n");
 }
-int checkFreeSpaces() 
+int checkFreeSpaces()
 {
 
 }
-void playerMove() 
+void playerMove()
 {
 
 }
-void computerMove() 
+void computerMove()
 {
 
 }
-char checkWinner() 
+char checkWinner()
 {
 
 }
-void printWinner(char winner) 
+void printWinner(char winner)
 {
 
 }
